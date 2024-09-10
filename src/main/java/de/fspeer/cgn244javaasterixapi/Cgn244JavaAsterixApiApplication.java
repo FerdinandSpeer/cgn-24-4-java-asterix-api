7c19a12ab2887ed651ebd86cfb6d8ce16aeb311c
ferdinandspeer <ferdinandspeer@gmail.com>
2024-09-10 11:04:30 +0200

    Initial commit

---
+++ src/main/java/de/fspeer/cgn244javaasterixapi/Cgn244JavaAsterixApiApplication.java
@@ -0,0 +1,13 @@
+package de.fspeer.cgn244javaasterixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cgn244JavaAsterixApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cgn244JavaAsterixApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/fspeer/cgn244javaasterixapi/Cgn244JavaAsterixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fspeer.cgn244javaasterixapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cgn244JavaAsterixApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
